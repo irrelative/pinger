2a276e60477540e239c832ce4f4625fb4e947057
Justin Davis <justin.davis@unsupervised.com>
2020-12-08 17:04:11 -0700

    logging, and catch errors

--- pinger.py
+++ pinger.py
@@ -1,10 +1,19 @@
+import logging
 import time
 
 import icmplib
 import psycopg2
 
-HOSTS = ['boxily.com', '192.168.4.1']
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler()
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
+
+HOSTS = ['boxily.com', '192.168.4.1']
 
 def main():
     conn = psycopg2.connect('dbname=pi')
@@ -12,8 +21,13 @@ def main():
     while True:
         start = time.time()
         for host in HOSTS:
-            resp = icmplib.ping(host, count=1, privileged=False)
-            cur.execute("INSERT INTO pings (host, is_alive, duration) VALUES (%s, %s, %s)", (host, resp.is_alive, resp.max_rtt))
+            try:
+                resp = icmplib.ping(host, count=1, privileged=False)
+            except Exception as ex:
+                logger.error("Error: %s", ex)
+            else:
+                logger.info("%s %s %s", host, resp.is_alive, resp.max_rtt)
+                cur.execute("INSERT INTO pings (host, is_alive, duration) VALUES (%s, %s, %s)", (host, resp.is_alive, resp.max_rtt))
         conn.commit()
         time.sleep(max(0, 1 - (time.time() - start)))
 
